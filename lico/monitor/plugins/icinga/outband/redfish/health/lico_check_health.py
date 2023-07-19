81b43c7e4cf31e692be29541dd096ede08d1e914
daixw3 <daixw3@lenovo.com>
2023-07-19 10:53:33 +0800

    [JIRA:LICO-7294]Modify args

--- lico/monitor/plugins/icinga/outband/redfish/health/lico_check_health.py
+++ lico/monitor/plugins/icinga/outband/redfish/health/lico_check_health.py
@@ -16,8 +16,6 @@ import argparse
 import json
 from enum import IntEnum
 
-from redfish.rest.v1 import RetriesExhaustedError
-
 from lico.monitor.plugins.icinga.helper.base import MetricsBase, PluginData
 from lico.monitor.plugins.icinga.outband.redfish.common import (
     RedfishConnection,
@@ -125,39 +123,27 @@ def get_health_info(plugin_data, conn, args):
 
 
 def parse_command_line():
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter, add_help=False)
-
-    group = parser.add_argument_group(title="require arguments")
-    group.add_argument('--host', help="BMC IP address;")
-    group.add_argument('--username', help="BMC login user;")
-    group.add_argument('--password', help="BMC login password;")
-
-    group = parser.add_argument_group(title="optional arguments")
-    group.add_argument('--res_instance', default='Systems', help="""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', help="BMC IP address;", required=True)
+    parser.add_argument('--username', help="BMC login user;", required=True)
+    parser.add_argument('--password', help="BMC login password;",
+                        required=True)
+    parser.add_argument('--res_instance', default='Systems', help="""
     resource instances, default is Systems;
     """)
-    group.add_argument('--res_type', default='ActiveLog', help="""
+    parser.add_argument('--res_type', default='ActiveLog', help="""
     Resource type, default is ActiveLog;
     """)
-    group.add_argument('--timeout', default=5, type=int, help="""
+    parser.add_argument('--timeout', default=5, type=int, help="""
     Timeout in seconds, default is 5s;
     """)
-    group.add_argument('--max_attempt', default=1, type=int, help="""
+    parser.add_argument('--max_attempt', default=1, type=int, help="""
     Max attempt times, default is 1;
     """)
-    group.add_argument('--verbose', action='store_true', help="""
+    parser.add_argument('--verbose', action='store_true', help="""
     Verbose mode;
     """)
-    group.add_argument("-h", "--help", action='store_true',
-                       help="show this help message and exit")
-
     result = parser.parse_args()
-    if result.help:
-        parser.print_help()
-        print("")
-        exit(0)
-
     return result
 
 
@@ -167,11 +153,8 @@ if __name__ == '__main__':
     try:
         with RedfishConnection(args) as conn:
             get_health_info(plugin_data, conn, args)
-    except RetriesExhaustedError:
-        if args.verbose:
-            print("The maximum number of attempts has been reached.")
     except Exception as e:
         if args.verbose:
-            print(e)
+            raise e
     finally:
         plugin_data.exit()
